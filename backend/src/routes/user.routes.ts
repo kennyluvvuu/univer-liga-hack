e15a5832f0faa43b2aaa1aa6d733f74d26ef489f
Sergey Sokolovsky <sergey270307@gmail.com>
2026-02-28 21:43:37 +0700

    add new routes

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -2,12 +2,35 @@ import Elysia from "elysia";
 import UserService from "../services/user.serivce";
 import CommentService from "../services/comment.service";
 import { ReviewSchema } from "../schemas/review.schema";
+import { authPlugin } from "../plugins/auth.plugin";
+import TaskService from "../services/task.service";
 
 export const userRoutes = (
     userService: UserService,
     commentService: CommentService,
+    tasksService: TaskService,
 ) =>
     new Elysia()
+        .use(authPlugin)
+        .get("/me", async ({ payload, status }) => {
+            if (!payload || !payload.sub)
+                return status(401, {
+                    error: "Unauthorized",
+                });
+            const userData = await userService.getById(payload.sub);
+            if (!userData) return status(404, "Not Found");
+            return userData;
+        })
+        .get("/tasks", async ({ payload, status }) => {
+            if (!payload || !payload.sub)
+                return status(401, {
+                    error: "Unauthorized",
+                });
+            const userTasks = await tasksService.getByUserId(payload.sub);
+            if (userTasks.length < 0)
+                return status(404, { message: "У вас нет задач" });
+            return userTasks;
+        })
         .get("/employees", async () => {
             return userService.list();
         })
@@ -23,7 +46,7 @@ export const userRoutes = (
                 const avaliableReview = await commentService.getByTaskId(
                     body.taskId,
                 );
-                if (avaliableReview)
+                if (avaliableReview.length > 0)
                     return status(409, {
                         message: "Вы уже оставили отзыв по этой задаче",
                     });
